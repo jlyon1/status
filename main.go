b5670366285589ff4a3116486bbebe58e206d75f
jlyon1 <joeylyon1@gmail.com>
2017-11-23 18:17:09 -0500

    Restructure for overhaul

--- main.go
+++ main.go
@@ -3,75 +3,14 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"time"
-	"fmt"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
+
 )
-
-var clients []*websocket.Conn
-
-var msg string
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func sendUpdate(fromClient *websocket.Conn) {
-	for i, client := range clients {
-		err := client.WriteMessage(1, []byte(msg))
-
-		if err != nil {
-			clients = append(clients[:i], clients[i+1:]...)
-			log.Println(err)
-			continue
-		}
-	}
-
-}
-
-func closeClient(c *websocket.Conn) {
-	log.Println("close requested")
-	c.Close()
-}
-
-func addClient(c *websocket.Conn) {
-	clients = append(clients, c)
-	go processMessages(c)
-}
-
-func processMessages(c *websocket.Conn) {
-	defer closeClient(c)
-	sendUpdate(c)
-	for {
-		sendUpdate(c)
-		time.Sleep(1000 * time.Millisecond)
-
-	}
-}
-
-func ReceiveClient(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	} else {
-		go addClient(c)
-	}
-}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
-}
-
-func UpdateHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "update.html")
 }
 
 func WriteJSON(w http.ResponseWriter, data interface{}) error {
@@ -85,38 +24,11 @@ func WriteJSON(w http.ResponseWriter, data interface{}) error {
 	return nil
 }
 
-type Data struct {
-	Message   string `json: "message"`
-	Password  string `json: "password"`
-}
-
-func SetHandler(w http.ResponseWriter, r *http.Request) {
-	test := Data{}
-	err := json.NewDecoder(r.Body).Decode(&test)
-	if(err != nil){
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	fmt.Printf("%v",test.Password)
-	if(test.Password == "password"){
-		msg = test.Message
-		fmt.Println("good job")
-	}
-}
-
-
 func main() {
 	r := mux.NewRouter()
-	fs := http.FileServer(http.Dir("static"))
 	r.HandleFunc("/", IndexHandler).Methods("GET")
-	r.HandleFunc("/update", UpdateHandler).Methods("GET")
-	r.HandleFunc("/update/submit/", SetHandler).Methods("POST")
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 
-	r.Handle("/static/app.js", http.StripPrefix("/static/", fs))
-	r.Handle("/static/styles.css", http.StripPrefix("/static/", fs))
-	r.HandleFunc("/ws", ReceiveClient)
-
-	// Serve requests
 	http.Handle("/", r)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
--- static/app.js
+++ static/app.js
@@ -1,29 +1,4 @@
-var textbox = Vue.component('text-box',{
-  template:`<div class="message_body">
-  <div> <input v-model="message" placeholder="Enter a msg"/> </div>
-  <div style="margin-top: 10px;"> <input type="password" v-model="password" placeholder=""/> </div>
-  <div style="margin-top: 10px; float:right;"> <button v-on:click="send">Submit</button> </div>
-  </div>`,
-  data (){
-    return {
-      message: "Hello World",
-      password: "",
-      counter: 0,
-      ws: {}
-    }
-  },
-  methods: {
-    send: function(){
-      console.log("asdf");
-      var val = {
-        message: this.message, password: this.password
-      }
-      $.post("update/submit/",JSON.stringify(val),function(data){
-        console.log(data)
-      })
-    }
-  }
-})
+
 var msgbdy = Vue.component('message-body',{
   template:`<div class="message_body">{{msgtxt}}</div>`,
   data (){
@@ -33,44 +8,26 @@ var msgbdy = Vue.component('message-body',{
     }
   },
   methods:{
-    openWebsocket: function(){
-      if (ws) {
-        return false;
-      }
-      var ws = new WebSocket("ws://127.0.0.1:8080/ws");
 
-      ws.onopen = function(evt) {
-        console.log("ya did it")
-      }
-      ws.onclose = function(evt) {
-        ws = null;
-      }
-      var el = this;
-      ws.onmessage = function(evt) {
-        console.log(this.msgtxt);
-        el.msgtxt = evt.data;
-      }
-      ws.onerror = function(evt) {
-        console.log(evt.data)
-      }
-
-    }
   },
   mounted (){
-    this.openWebsocket();
+
   }
 
 })
 
-var titlebar = Vue.component('title-bar',{
-  props: ['msg'],
-  template:`<div class="titlebar">{{msg}}</div>`,
+Vue.component("title-bar",{
+  props: ['txt'],
+  template: `<div v-bind:style=titleStyle><p v-bind:style=paragraphStyle>{{txt}}</p></div>`,
   data (){
-    return {
+    return{
+      titleStyle: {position:"absolute",backgroundColor:"#eee",height:"34px",width:"auto",top:"1",left:"0",right:"0"},
+      paragraphStyle: {float: "left",height:"34px",lineHeight:"34px",verticalAlign:"center",paddingLeft:"30px",margin:"0"},
+      titleText: "Joseph Lyon"
     }
   }
 
-})
+});
 
 var ShuttleTracker = new Vue({
   el: '#app-vue',
